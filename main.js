b1101d6da38b8d962956851d1158be8c334eb2c1
DebordCompany <debord.info@gmail.com>
2022-08-02 13:41:49 +0200

    creados con exito header y portada

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const menu = document.querySelector("#nav");
+const btn  = document.querySelector("#boton")
+
+
+btn.addEventListener("click", accion
+)
+
+function accion (){
+   menu.classList.toggle("activo")
+}
