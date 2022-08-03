73e5e8111ed28fa52cd2c9536720810670cd9889
DebordCompany <debord.info@gmail.com>
2022-08-03 15:24:37 +0200

    añadido formulario, seccion distribuidora y mas o menos todo en responsive, hay que cambiar colores y encapcularlos en variables asi como los padding y margins generales. Hay que continuar con el footer y ver com ohacer el resto de las paginas

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const btn  = document.querySelector("#boton")
 
 btn.addEventListener("click", accion
 )
-
+menu.addEventListener("click", accion)
 function accion (){
    menu.classList.toggle("activo")
 }
